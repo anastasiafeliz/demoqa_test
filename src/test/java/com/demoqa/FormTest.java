127bc352bf71a965a69624c9b085e73c81e2244d
anastasia <piganova.nastya@gmail.com>
2022-04-06 15:56:49 +0300

    added .form-test

--- src/test/java/com/demoqa/FormTest.java
+++ src/test/java/com/demoqa/FormTest.java
@@ -7,37 +7,36 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
+
 public class FormTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1024x768";
+
     }
 
     @Test
     void fillFormTest() {
+
         Selenide.open("/automation-practice-form");
-        Selenide.executeJavaScript("document.querySelector(\"footer\").hidden = 'true';" +
-                "document.querySelector(\"#fixedban\").hidden = 'true'");
+        Selenide.executeJavaScript("document.querySelector(\"footer\").hidden = 'true';document.querySelector(\"#fixedban\").hidden = 'true'");
         $("[id=firstName]").setValue("Anastasia");
         $("[id=lastName]").setValue("Piganova");
-        Selenide.$("[id=userEmail]").setValue("piganova@io.com");
-        Selenide.$("[id=userNumber]").setValue("5555555");
-        Selenide.$("[id=dateOfBirthInput]").click();
-        Selenide.$("[id=react-datepicker__year-select]").selectOption("1998");
-        Selenide.$("[id=react-datepicker__month-select]").selectOption("June");
-        Selenide.$("[id=react-datepicker__day-select]").selectOption("15");
-        Selenide.$("[id=subjects-label]").click();
-        Selenide.$("[id=subjects-label]").selectOption("Chemistry");
-        Selenide.$("[id=hobbiesWrapper]").click();
-        Selenide.$("[id=hobbiesWrapper]").selectOption("Reading");
-        Selenide.$("[id=uploadPicture]").uploadFromClasspath("photo.jpg");
-        Selenide.$("[id=currentAddress]").setValue("Ulitsa 34");
-        Selenide.$("[byText=state]").click();
-        Selenide.$("[byText=city]").click();
-        Selenide.$("[id=submit]").click();
-        Selenide.$("[id=submit]").shouldBe(Condition.visible).click();
+        $("[id=userEmail]").setValue("piganova@io.com");
+        $("[id=userNumber]").setValue("5555555");
+        $("[id=subjects-label]").click();
+        $("[id=subjects-label]").selectOption("Chemistry");
+        $("[id=hobbiesWrapper]").click();
+        $("[id=hobbiesWrapper]").selectOption("Reading");
+        $("[id=uploadPicture]").uploadFromClasspath("photo.jpg");
+        $("[id=currentAddress]").setValue("Ulitsa 34");
+        $("[byText=state]").click();
+        $("[byText=city]").click();
+        $("[id=submit]").click();
+        $("[id=submit]").shouldBe(Condition.visible).click();
 
     }
 }
