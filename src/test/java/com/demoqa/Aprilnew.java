54a4a833896762b739bf7dcd7df074b915616150
anastasia <piganova.nastya@gmail.com>
2022-04-12 11:33:45 +0300

    added new info

--- src/test/java/com/demoqa/Aprilnew.java
+++ src/test/java/com/demoqa/Aprilnew.java
@@ -3,18 +3,14 @@ package com.demoqa;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.conditions.Text;
 import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
 import java.io.File;
-
 import static com.codeborne.selenide.Selenide.$;
 
-public class aprilnew {
+public class Aprilnew {
     @BeforeAll
 static void beforeAll() {
     Configuration.holdBrowserOpen = true;
@@ -22,9 +18,7 @@ static void beforeAll() {
     Configuration.browserSize = "1024x768";
 }
     @Test
-    void fillFormTest() {}
-
-    {
+    void fillFormTest() {
     Selenide.open("/automation-practice-form");
     Selenide.executeJavaScript("document.querySelector(\"footer\").hidden = 'true';document.querySelector(\"#fixedban\").hidden = 'true'");
 
@@ -32,18 +26,18 @@ static void beforeAll() {
         $("[id=lastName]").setValue("Piganova");
         $("[id=userEmail]").setValue("piganova@io.com");
 
-       // $("#gender-radio-3").parent().click();//не срабатывает
-        $("[id=genterWrapper]").$(new ByText("Other")).click();
+        $("[id=genterWrapper]").$(new ByText("Female")).click();
         $("[id=userNumber]").setValue("1234567890");
+
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("1");
         $(".react-datepicker__year-select").selectOptionByValue("1988");
-        //$(".react-datepicker__day—select").click();//не срабатывает
+        $(".react-datepicker__day").selectOptionByValue("25");
+        $("#subjectsInput").click();
+        $("#subjectsInput").setValue("Chemistry").pressEnter();
+        $(new ByText("Reading")).click();
 
-        $("#subjectsInput").setValue("Chemistry").pressEnter();//не срабатывает
-        $("#hobbies-checkbox-3").parent().click(); //не срабатывает
-
-        $("#uploadPicture").uploadFile(new File("src/test/resourses/123.jpg"));
+        $("#uploadPicture").uploadFile(new File("src/test/resources/123.jpg"));
 
         $("[id=currentAddress]").setValue("Ulitsa 34");
         $("#state").click();
